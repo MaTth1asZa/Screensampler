1792c9a0645d1ddd0ae22fa0a39e865a81585599
Matthew Verbeek <mattverbeek@gmail.com>
2022-03-17 21:51:07 +0200

    got image sampling working!

--- screensamplerfirsttry.py
+++ screensamplerfirsttry.py
@@ -8,24 +8,35 @@ Created on Thu Mar 17 17:45:23 2022
 
 from mss import mss
 import mss.tools
-print('hello world')
+from colorthief import ColorThief
+from PIL import Image
+import io
+
+
 #with mss() as sct:
    # for firstscreenshot in sct.save():
      #   print(firstscreenshot)
-        
+
 with mss.mss() as sct:
+    Topbar=[]
     x=0
     for i in range(16):
         
         # The screen part to capture
         monitor = {"top": 0, "left": 0+x, "width": 160, "height": 160}
         output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+        quadrant=sct.grab(monitor)
+        mss.tools.to_png(quadrant.rgb, quadrant.size, output=output)
         
-        Topbar=[]
-        Topbar.append(sct.grab(monitor))
+        #f = Image.open("sct-{top}x{left}_{width}x{height}.png".format(**monitor))
+        quadrantcolor=ColorThief("sct-{top}x{left}_{width}x{height}.png".format(**monitor))
+        quadrantDominant=quadrantcolor.get_color(quality=1)
+        Topbar.append(quadrantDominant)
+        print(quadrantDominant)
+    
         x=x+160
        
-        print("This is the picture array", Topbar)
+      
  
 
         # Save to the picture file
@@ -41,4 +52,3 @@ with mss.mss() as sct:
         # Save to the picture file
        # mss.tools.to_png(sct_img01.rgb, sct_img01.size, output=output)
         #print(output)
-        
